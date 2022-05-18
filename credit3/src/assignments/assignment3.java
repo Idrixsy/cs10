06b360be432046d684648ee8502d7de3023a0e9f
1100038064 <1100038064@L815WD220027.edu.cbe.ab.ca>
2022-05-18 13:27:08 -0600

    assignment3

---
+++ credit3/src/assignments/assignment3.java
@@ -0,0 +1,31 @@
+package assignments;
+
+public class assignment3 {
+
+	public static void main(String[] args) 
+	{
+ System.out.println("B     I     N     G     O");
+ System.out.println("1     19    32    46    61");
+ System.out.println("6     30    35    49    64");
+ System.out.println("13    21    Free  52    69");
+ System.out.println("16    27    42    58    71");
+ System.out.println("9     23    45    60    75");
+		    }
+	{
+		}
+
+		/* Screen Dump
+		 
+		B     I     N     G     O
+		1     19    32    46    61
+		6     30    35    49    64
+		13    21    Free  52    69
+		16    27    42    58    71
+		9     23    45    60    75
+
+		 
+		 */
+
+	}
+
+
