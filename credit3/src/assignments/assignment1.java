89549ddd54ef81cf6afe8514d41b71647e24b271
1100038064 <1100038064@L815WD220027.edu.cbe.ab.ca>
2022-05-18 13:03:16 -0600

    1:03PM

---
+++ credit3/src/assignments/assignment1.java
@@ -0,0 +1,31 @@
+package assignments;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sami Kazoun");
+		System.out.println("From Mr.Abdalla ");
+		System.out.println("Addy");
+		System.out.println("Today Date");
+		System.out.println("Sami The chosen one for you");
+		
+		System.out.println(2 * 100);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ credit3/src/assignments/assignment2.java
@@ -0,0 +1,71 @@
+package assignments;
+
+public class assignment2 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println ("*"+"*"+"*"+"*"+"*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*");
+		System.out.println("*"+"*"+"*"+"*"+"*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println                     ("\t*");
+		System.out.println ("*"+"*"+"*"+"*"+"*");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
